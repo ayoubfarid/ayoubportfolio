074394abe9e84596730691b41eadde16e1092a57
ayoubfarid <ayoubfarid80@gmail.com>
2021-09-13 13:46:04 +0100

    add deployement

---
+++ next.config.js
@@ -0,0 +1,14 @@
+
+  /**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    /* config options here */
+    images: {
+        loader: "imgix",
+        path: "https://noop/",
+    },
+
+  }
+  
+  module.exports = nextConfig
